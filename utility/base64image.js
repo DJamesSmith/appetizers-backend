134e5b367361a8c79548b6a2976f1438635d0bd3
Dion James Smith <dion.smith@live.com>
2025-11-01 17:11:12 +0530

    removed log console from base64 file

--- utility/base64image.js
+++ utility/base64image.js
@@ -3,7 +3,6 @@
 const fs = require('fs')
 
 const imageToBase64 = filePath => {
-    console.log(`Entered imageToBase64`)
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, { encoding: 'base64' }, (err, data) => {
             if (err) {
